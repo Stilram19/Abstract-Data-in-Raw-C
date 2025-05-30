fa3b7a5ec2a596e32e24eaf1cbc95c51fa0ea59a
Stilram19 <bednaouiomar19@gmail.com>
2025-05-30 15:52:37 +0100

    transitioning into a generic solution of dynamic array & applying OOP in C

--- data_structures/header/dynamic_array.h
+++ data_structures/header/dynamic_array.h
@@ -1,31 +1,26 @@
 #ifndef DYNAMIC_ARRAY_H
 # define DYNAMIC_ARRAY_H
 
+/* this is the dynamic array's interface */
+
 #include <stddef.h>
 
+/* data structure declaration */ 
+typedef struct DynamicArray DynamicArray;
 
-// this is the dynamic array's interface
+/* constructor */
+DynamicArray* da_create(size_t initial_capacity, size_t elem_size);
 
-// data (attributes)
-typedef struct {
-    int* data;
-    size_t size;
-    size_t capacity;
-} DynamicArray;
-
-// constructor
-DynamicArray* da_create(size_t initial_capacity);
-
-// destructor
+/* destructor */
 void da_destroy(DynamicArray* arr);
 
-// public utilities to manipulate the structure
-int da_get(DynamicArray* arr, size_t index);
-void da_set(DynamicArray* arr, size_t index, size_t value);
-void da_push_back(DynamicArray* arr, size_t value);
-void da_insert_at(DynamicArray* arr, size_t index, size_t value);
-void da_remove_at(DynamicArray* arr, size_t index);
-size_t da_capacity(DynamicArray* arr);
-size_t da_size(DynamicArray* arr);
+/* public utilities to manipulate the structure */
+void *da_get(DynamicArray *arr, size_t index);
+void da_set(DynamicArray *arr, size_t index, void *value);
+void da_push_back(DynamicArray *arr, void *value);
+void da_insert_at(DynamicArray *arr, size_t index, void *value);
+void da_remove_at(DynamicArray *arr, size_t index);
+size_t da_capacity(DynamicArray *arr); /* returns the capacity of the given dynamic array */
+size_t da_size(DynamicArray *arr); /* returns the size of the given dynamic array */
 
 #endif
