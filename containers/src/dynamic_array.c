0a06247bc606c53ac8658a70ddd5e82af1d5b56e
Stilram19 <bednaouiomar19@gmail.com>
2025-06-02 18:07:53 +0100

    the entire dynamic array interface was implemented & tests in next commit

--- containers/src/dynamic_array.c
+++ containers/src/dynamic_array.c
@@ -7,7 +7,7 @@
 # include <string.h>
 
 
-/* private attributes (encapsulation) */
+/* private attributes */
 typedef struct DynamicArray {
     void* data;           // raw byte buffer
     size_t size;          // number of elements
@@ -17,27 +17,90 @@ typedef struct DynamicArray {
 
 /* private helpers */
 
-/* reallocates the memory of the underlying data to newCapacity 
+/* reallocates the memory of the underlying data to new_capacity 
 ** if reallocation went successful, both arr->data and arr->capacity are changed
 ** otherwise nothing is changed
 ** returns a pointer to the new allocated memory if reallocation went successful,
 ** returns NULL otherwise to indicate that the reallocation failed
 */
-static void *helper_reallocate(DynamicArray *arr, size_t newCapacity) {
+static void *helper_reallocate(DynamicArray *arr, size_t new_capacity) {
     if (arr == NULL) {
         return (NULL);
     }
 
-    assert(newCapacity > 0);
+    assert(new_capacity > 0);
 
-    void *newData = realloc(arr->data, newCapacity * arr->elem_size); 
+    void *newData = realloc(arr->data, new_capacity * arr->elem_size); 
     if (newData == NULL) {
         return (NULL);
     }
     arr->data = newData;
-    arr->capacity = newCapacity;
+    arr->capacity = new_capacity;
 
     return (newData);
+}
+
+/* returns the offset in bytes (in terms of the element size) */
+static size_t helper_get_offset_in_bytes(DynamicArray *arr, size_t offset) {
+    if (arr == NULL) {
+        return (offset);
+    }
+
+    return (offset * arr->elem_size);
+}
+
+/*
+ ** this helper right shifts the array by one, starting from startIndex
+ ** if arr is NULL or arr->data is NULL, nothing happens
+ ** size must be less than capacity
+ ** startIndex needs to be less than the size
+ ** if call is successful arr->size is incremented
+*/
+static void helper_right_shift(DynamicArray *arr, size_t startIndex) {
+    if (arr == NULL || arr->data == NULL) {
+        return ;
+    }
+
+    assert(arr->size < arr->capacity);
+    assert(startIndex < arr->size);
+
+    size_t srcOffset = helper_get_offset_in_bytes(arr, startIndex);
+    size_t destOffset = helper_get_offset_in_bytes(arr, startIndex + 1);
+    size_t length = helper_get_offset_in_bytes(arr, arr->size - startIndex);
+    void *srcPtr = (char*)arr->data + srcOffset; 
+    void *destPtr = (char*)arr->data + destOffset; 
+
+    memmove(destPtr, srcPtr, length);
+    arr->size++;
+}
+
+/*
+ ** this helper left shits elements one time, starting from startIndex
+ ** if arr is NULL or arr->data is NULL, nothing happens
+ ** startIndex must be less than or equals arr->size and greater than 0
+ ** if call is successful arr->size is decremented
+*/
+static inline void helper_left_shift(DynamicArray *arr, size_t startIndex) {
+    if (arr == NULL || arr->data == NULL) {
+        return ;
+    }
+
+    assert(startIndex <= arr->size);
+    assert(startIndex > 0);
+
+    arr->size--;
+
+    if (startIndex == arr->size) {
+        return ;
+    }
+
+    size_t srcOffset = helper_get_offset_in_bytes(arr, startIndex);
+    size_t destOffset = helper_get_offset_in_bytes(arr, startIndex - 1);
+    size_t length = helper_get_offset_in_bytes(arr, arr->size - startIndex);
+    void *srcPtr = (char*)arr->data + srcOffset;
+    void *destPtr = (char*)arr->data + destOffset;
+
+    memcpy(destPtr, srcPtr, length);
 }
 
 /*
@@ -86,25 +149,23 @@ void da_destroy(DynamicArray *arr) {
 
 /*
 ** returns generic pointer to the value at the index
-** index cannot be greater than or equals the array size
+** if arr is NULL or arr->data is NULL or index is greater than arr->size, NULL is returned 
 */
 void *da_get(DynamicArray *arr, size_t index) {
-    if (arr == NULL || arr->data == NULL) {
-        return (0);
+    if (arr == NULL || arr->data == NULL  || index >= arr->size) {
+        return (NULL);
     }
 
-    /* index must be less than arr->size */
-    assert(index < arr->size);
-
-    size_t indexOffset = index * arr->elem_size; /* the arithmetic step size */
+    size_t indexOffset = helper_get_offset_in_bytes(arr, index); /* index in bytes */
 
     return (((char*)arr->data + indexOffset));
 }
 
 /* adds the value at the end of the dynamic array 
 ** if array is full we reallocate twice the current capacity (amortized cost)
+** the size is incremented, if the push_back call succeeds
 */
-void da_push_back(DynamicArray *arr, void *ptrToValue) {
+void da_push_back(DynamicArray *arr, void *ptr_to_value) {
     if (arr == NULL || arr->data == NULL) {
         return ;
     }
@@ -120,19 +181,20 @@ void da_push_back(DynamicArray *arr, void *ptrToValue) {
         }
     }
 
-    size_t indexOffset = arr->size * arr->elem_size; /* arithmetic step size */
+    size_t indexOffset = helper_get_offset_in_bytes(arr, arr->size); /* index in bytes */
 
-    /* copy elem_size bytes from the memory block pointed to by 'ptrToValue', to the back of the array */
-    memcpy((char*)arr->data + indexOffset, ptrToValue, arr->elem_size);
+    /* copy elem_size bytes from the memory block pointed to by 'ptr_to_value', to the back of the array */
+    memcpy((char*)arr->data + indexOffset, ptr_to_value, arr->elem_size);
 
     arr->size++;
 }
 
 /*
 ** sets the given value in the given index
-** if arr is NULL, arr->data is NULL or index is out of range, then nothing is changed 
+** if arr is NULL, arr->data is NULL, then nothing is changed 
+** the index must be less than the array size
 */
-void da_set(DynamicArray *arr, size_t index, void *ptrToValue) {
+void da_set(DynamicArray *arr, size_t index, void *ptr_to_value) {
     if (arr == NULL || arr->data == NULL) {
         return ;
     }
@@ -140,10 +202,66 @@ void da_set(DynamicArray *arr, size_t index, void *ptrToValue) {
     /* index must be less than the array size */
     assert(index < arr->size);
 
-    size_t indexOffset = index * arr->elem_size; /* arithmetic step size */
+    size_t indexOffset = helper_get_offset_in_bytes(arr, index); /* index in bytes */
 
-    /* copy elem_size bytes from the memory block pointed to by 'ptrToValue', to the back of the array */
-    memcpy((char*)arr->data + indexOffset, ptrToValue, arr->elem_size);
+    /* copy elem_size bytes from the memory block pointed to by 'ptr_to_value', to the index*/
+    memcpy((char*)arr->data + indexOffset, ptr_to_value, arr->elem_size);
+}
+
+/*
+ ** inserts the element pointed to by ptr_to_value at the right index
+ ** if arr is NULL or arr->data is NULL, nothing will happen
+ ** index must be less than or equals the array's size
+*/
+void da_insert_at(DynamicArray *arr, size_t index, void *ptr_to_value) {
+    if (arr == NULL || arr->data == NULL) {
+        return ;
+    }
+
+    /* index cannot be greater than size */
+    assert(index <= arr->size);
+
+    /* if index is size, it means we need to insert at the back of the array */
+    if (index == arr->size) {
+        da_push_back(arr, ptr_to_value);
+        return ;
+    }
+
+    /* size must not be greater than capacity (at any given time) */
+    assert(arr->size <= arr->capacity);
+
+    if (arr->size == arr->capacity) {
+        void *newData = helper_reallocate(arr, arr->capacity * 2);
+        if (newData == NULL) {
+            return ;
+        }
+    }
+
+    /* right shift all the elements */
+    helper_right_shift(arr, index); /* it also increments the array size */
+
+    size_t indexOffset = helper_get_offset_in_bytes(arr, index); /* index in bytes */
+
+    /* copy elem_size bytes from the memory block pointed to by 'ptr_to_value', to the index */
+    memcpy((char*)arr->data + indexOffset, ptr_to_value, arr->elem_size);
+}
+
+
+/*
+** removes the element at the index
+** if arr is NULL or arr->data is NULL, nothing happens
+** index must be less than the array size
+*/
+void da_remove_at(DynamicArray *arr, size_t index) {
+    if (arr == NULL || arr->data == NULL) {
+        return ;
+    }
+
+    /* index must be less than arr->size (valid index) */
+    assert(index < arr->size);
+
+    /* left shift */
+    helper_left_shift(arr, index + 1); /* it also decrements the array size */
 }
 
 /*
