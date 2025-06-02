311a9ba79b567653d0f2f3fe178e2de51f3d95b8
Stilram19 <bednaouiomar19@gmail.com>
2025-06-02 18:07:53 +0100

    the entire dynamic array interface was implemented & tests in next commit

--- containers/header/dynamic_array.h
+++ containers/header/dynamic_array.h
@@ -16,9 +16,9 @@ void da_destroy(DynamicArray* arr);
 
 /* public utilities to manipulate the structure */
 void *da_get(DynamicArray *arr, size_t index);
-void da_set(DynamicArray *arr, size_t index, void *value);
-void da_push_back(DynamicArray *arr, void *value);
-void da_insert_at(DynamicArray *arr, size_t index, void *value);
+void da_set(DynamicArray *arr, size_t index, void *ptrToValue);
+void da_push_back(DynamicArray *arr, void *ptrToValue);
+void da_insert_at(DynamicArray *arr, size_t index, void *ptrToValue);
 void da_remove_at(DynamicArray *arr, size_t index);
 size_t da_capacity(DynamicArray *arr); /* returns the capacity of the given dynamic array */
 size_t da_size(DynamicArray *arr); /* returns the size of the given dynamic array */
